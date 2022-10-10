f6f0140db7a2d002409cc2908216d3447bd97d9f
john <kiokoj81@gmail.com>
2022-10-10 17:38:50 +0300

    push todo list master code to the new branch for review

---
+++ src/modules/render.js
@@ -0,0 +1,23 @@
+/* eslint-disable*/
+import { todosArr, listBox } from "../index.js";
+import clearCompletedTodo from "./clear.js";
+
+export default function displayTodo() {
+  let htmlCode = "";
+  todosArr.forEach((todo, index) => {
+    htmlCode += `<div class="list-items" id= '${index}'>
+    
+    <i class="bi ${
+      todo.completed ? "bi-check-circle-fill" : "bi-circle"
+    }" data-action = "check" id="flex-start"></i> 
+    <p class= "${
+      todo.completed ? "completed" : ""
+    }" data-action = "edit" data-action = "check"> ${todo.description}</p>
+    <i class="bi bi-three-dots-vertical end hide"></i>
+    <i class="bi bi-trash flex-end delete-btn" data-action = "delete"></i>
+
+    </div>`;
+  });
+
+  listBox.innerHTML = htmlCode;
+}
